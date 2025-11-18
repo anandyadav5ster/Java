d33de2a76a40bbde8a104f7202f4a51c3980d24f
anand.yadav <anandkumar.yadav@tui.co.uk>
2025-11-18 12:02:31 +0530

    Generic method to reverse string

--- java/TestOne.java
+++ java/TestOne.java
@@ -13,4 +13,14 @@ class TestOne {
         }
         System.out.println(sum);
     }
+    // Generic method to reverse string
+    public static <T> void reverseItem(T input){
+        String item = input.toString();
+        char[] words = item.toCharArray();
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(char word : words){
+            hm.put(word,hm.getOrDefault(word,0)+1);
+        }
+        System.out.println(hm);
+    }
 }
