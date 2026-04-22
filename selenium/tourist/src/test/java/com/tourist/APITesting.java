7114b321f7c13e341a2c2c26d74bfec2c4d29f5c
anandyadav5ster <anandydav5ster@gmail.com>
2026-04-22 22:54:33 +0530

    patch request

--- selenium/tourist/src/test/java/com/tourist/APITesting.java
+++ selenium/tourist/src/test/java/com/tourist/APITesting.java
@@ -5,7 +5,7 @@ import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
+
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,6 +16,7 @@ public class APITesting {
 	public static void main(String[] args) {
 		Response response = null;
 		RequestSpecification request;
+		String systemId = null;
 		RestAssured.baseURI = "http://localhost:3001";
 
 		request = RestAssured.given();
@@ -35,13 +36,32 @@ if (allEmployees != null && !allEmployees.isEmpty()) {
         
         if (currentEmpId.equals("EMP04")) {
             // Extract the system ID (id) needed for the URL
-            String systemId = employee.get("id").toString();
-            
+            systemId = employee.get("id").toString();
+
+			// create payload, put request
+			Map<String, Object> newBook = new HashMap<>();
+
+			newBook.put("empId", "EMP04");
+			newBook.put("firstName", "Testing");
+			newBook.put("lastName", "Automation");
+			newBook.put("jobTitle", "QA");
+			response = request.contentType(ContentType.JSON).body(newBook).put("employees/"+systemId);
+			System.out.println("Status code "+response.statusCode());
+			System.out.println(" Check the existing employees "+statusCode+" "+response.asPrettyString());
+
+			// patch request
+			newBook.put("jobTitle", "QA Testing");
+			response = request.contentType(ContentType.JSON).body(newBook).patch("employees/"+systemId);
+			System.out.println("Status code "+response.statusCode());
+			System.out.println("Patch request "+statusCode+" "+response.asPrettyString());
+
             // Perform the delete using the systemId
             response = request.delete("employees/" + systemId);
             
             System.out.println("Deleted EMP04 (System ID: " + systemId + ") - Status: " + response.statusCode());
-        }
+        
+		
+		}
     }
 } else {
     System.out.println("No employees found or list is null.");
@@ -60,6 +80,8 @@ if (allEmployees != null && !allEmployees.isEmpty()) {
 	response = request.contentType(ContentType.JSON).body(newBook).post("/employees");
 	System.out.println("New employee created "+newBook);
 	System.out.println(response.statusCode());
+
+	
 	}
 	
 }	
